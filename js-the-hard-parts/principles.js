049c360ca6bd08bda7386d4f158aa321bc3c904a
Austin Beaufort <tinyhatcoding@gmail.com>
2019-02-05 21:20:03 -0500

    Initial Commit for this Course!

---
+++ js-the-hard-parts/principles.js
@@ -0,0 +1,36 @@
+// const num = 3;
+// function multiplyBy2(inputNumber) {
+//     const result = inputNumber * 2;
+//     return result;
+// }
+
+// console.log(multiplyBy2);
+// const output = multiplyBy2(4); // stores result in output
+// const newOutput = multiplyBy2(10); // stores result in newOutput
+
+// const name = 'Will';
+
+
+
+// FUNCTIONAL PROGRAMMING
+
+// FUNCTIONS ARE FIRST CLASS CITIZENS
+// VARIABLES CANNOT BE CHANGED // IMMUTABILITY
+
+// PURE FUNCTION (NO SIDE EFFECTS)
+// HIGHER ORDER FUNCTIONS.
+
+// function tenSquared() {
+//     return 10 * 10;
+// }
+
+// console.log(tenSquared());
+
+
+// CAN GENERALIZE THE SQUARE A NUMBER FUNCTION
+function squareNum(num) {
+    return num * num;
+}
+
+console.log(squareNum(10));
+console.log(squareNum(9));
