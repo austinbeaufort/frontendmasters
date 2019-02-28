183340803de350f025d1e3b83d59b3ec0e8cd7f6
Austin Beaufort <tinyhatcoding@gmail.com>
2019-02-28 11:55:58 -0500

    Notes on Security Principles

---
+++ js-the-good-parts/managingAsynchronicity/app.js
@@ -0,0 +1,52 @@
+// SYNCHRONOUS FUNCTIONS
+// do not return until the work has completed or has failed.
+
+// PROBLEMS WITH THREADS
+// Races
+// Deadlocks
+// Reliability
+// Performance.
+
+// MUTUAL EXCLUSION
+// Only one thread can be executing on a critical section at a time.
+// All other threads wanting to execute the critical section are blocked.
+// If threads don't interact, then the program runs at full speed.
+// If they do interact, then races will occur unless mutual exclusion is employed.
+
+// ASYNCHRONOUS FUNCTIONS
+// Return immediately. Success or failure will be determined,
+// somehow in the future.
+
+// TURN
+// A turn is started by an external event.
+// A callback function associated with the event is called,
+// and will run to completion.
+
+
+// ***** EVENT LOOP *****
+// EVENT LOOP (TURN BASED SYSTEM)
+
+// EVENT DRIVEN SYSTEMS
+
+// HYPERCARD
+
+// JAVASCRIPT IS MOVING TO THE SERVER.
+
+// WHAT SERVERS DO IS QUITE DIFFERENT FROM WHAT BROWSERS DO.
+
+// NODE.JS
+// implements a web server in a JavaScript event loop.
+
+
+// SERVERS
+
+// MESSAGE DRIVEN, MESSAGE QUEUE
+// SIMPLE EVENTS DON'T FIT.
+
+// FUNCTIONAL PROGRAMMING
+// FUTURES
+// PROMISES
+// MONADS
+// ARROWS
+// RX
+// FRP
