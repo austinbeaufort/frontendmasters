46b5d5fae362ae70f8dd51c4f5ea51880f78e933
Austin Beaufort <tinyhatcoding@gmail.com>
2019-03-24 12:02:49 -0400

    NVM vs. NPM, REPL, CLI Executable, Differences between Browser and NodesJS

--- introToNodeJS/notes.js
+++ introToNodeJS/notes.js
@@ -13,3 +13,51 @@
 // DESKTOP APPLICATIONS
 // INTERNET OF THINGS
 // LOTS OF OTHER THINGS
+
+
+// INSTALL NODE WITH NVM (NODE VERSION MANAGER)
+// ALLOWS TO RUN MANY VERSIONS OF NODE WHENEVER
+
+// INTERACTIVE REPL (PLAYGROUND ENVIRONMENT)
+// just type in "node" command to the terminal and can, 
+// begin writing JavaScript
+
+
+
+// CLI (COMMAND LINE INTERFACE) EXECUTABLE (FOR EVERYTHING ELSE)
+// Use to execute your node apps
+// node path/to/your/file.js
+
+// PRACTICE USING NODE CLI EXECUTABLE
+
+// const firstName = 'Austin';
+// const lastName = 'Beaufort';
+// const mood = 'Happy';
+
+// let sentence = `Hi I am ${firstName} ${lastName} and I am ${mood} today!`;
+// console.log(sentence);
+
+//FOR BROWSER..
+// DON'T ATTACH THINGS TO THE WINDOW, SECURITY RISK.
+
+
+// BROWSER VS. NODEJS
+
+// BROWSER
+// 1. build interactive apps for the web
+// 2. DOM
+// 3. Window
+// 4. Fragmentation
+// 5. Optional Modules
+// 6. Cannot access filesystem
+// 7. Async
+
+// NODE JS
+// 1. build server side apps and scripts
+// 2. Nope, no Gui (can virtualize)
+// 3. No window, but does have a global
+// 4. Versioned, no fragmentation
+// 5. Required Modules (commonjs +)
+// 6. Can access filesystem
+// 7. Async
+// 7. No browser based API's
